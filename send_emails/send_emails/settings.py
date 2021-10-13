88a2f76a3b9d3e4496ede578ca0b65f583fff4e1
BILL GRAHAM <grahambill011@gmail.com>
2021-10-13 04:42:11 +0300

    Update settings.py

--- send_emails/send_emails/settings.py
+++ send_emails/send_emails/settings.py
@@ -127,11 +127,11 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-DEFAULT_FROM_EMAIL = 'grahambill022@gmail.com'
+DEFAULT_FROM_EMAIL = ''
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'grahambill022@gmail.com'
+EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
